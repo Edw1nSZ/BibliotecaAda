60a389fa6837a4c566e3e121d5952ec1cf56d1f7
Edw1nSZ <edwincruz2basico@gmail.com>
2024-06-03 10:10:11 -0600

    Add files via upload

---
+++ EjercicioLibro/src/ejerciciolibro/EjercicioLibro.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ejerciciolibro;
+
+/**
+ *
+ * @author PC17LAB1
+ */
+public class EjercicioLibro {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
